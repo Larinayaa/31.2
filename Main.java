b3744fed8dd6fc984caa993e6b185b4dab080c8e
Larinayaa <kovalcukanna10@gmail.com>
2024-10-28 19:29:14 +0200

    end

--- Main.java
+++ Main.java
@@ -1,2 +1,28 @@
-public class Main {
-}
+/*
+Hanna Kovalchuk gr 53
+ */
+import java.util.Arrays;
+import java.util.List;
+
+    public class Main {
+        public static void main(String[] args) {
+            Student[] students = {
+                    new Student("Лиза", List.of(5,9,1,7,8,9)),
+                    new Student("Вова", List.of(6,9,11,4,5)),
+                    new Student("Катя", List.of(8,9,5,9,10,7)),
+                    new Student("Саша", List.of(4,7,6,5,5,9))
+            };
+            System.out.println("Перечень студентов...");
+            print(students); //просто вывод студентов
+            Arrays.sort(students);
+            System.out.println("Происходит сортировка по возрастанию среднего балла...");
+            print(students);
+            System.out.println("Сортировка окончена.");
+        }
+
+        public static void print(Student[] students){
+            for (Student student:students){
+                System.out.println(student);
+            }
+        }
+    }
--- Student.java
+++ Student.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
 import java.util.List;
-
-public class Student {
+public class Student implements Comparable<Student>{
     private String name;
     private List<Integer> rates;
+    private double number;
+    public Student(String name, List<Integer> rates) {
+        this.name = name;
+        this.rates = new ArrayList<>(rates);
+        this.number = ratingOcenok();
+    }
+    @Override
+    public String toString() {
+        return "Студент: " + name +
+                ". Оценки: " + rates + ". (Средний балл: " + number +")";
+    }
+    public double getNumber() {
+        return number;
+    }
+    private double ratingOcenok(){ //вычисление среднего балла
+        if (rates==null || rates.size()==0){
+            return 0;
+        }
+        double summa = 0.0;
+        for (Integer i : rates){
+            summa +=i;
+        }
+        return summa /rates.size();
+    }
+    @Override
+    public int compareTo(Student otherStudent) {
+        return Double.compare(number, otherStudent.number);
+    }
 }
